627bf9bfc9c1b5945f6518520aa7dd00feabd230
Bibiana <b.gamba10@uniandes.edu.co>
2018-03-31 16:44:08 -0500

    Mini changes in home '''''

--- imports/ui/Home.js
+++ imports/ui/Home.js
@@ -104,25 +104,23 @@ class Home extends Component {
 
 		return (
 			<div id="home-city">
-			<div id="box">
-				<h2>Please select the city you are in</h2>
-				<div id="chooser">
-				<h4>You can then begin to ask the questions you need</h4>
-			
-				
-				<Autosuggest 
-				
-					suggestions={suggestions}
-					onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-					onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-					getSuggestionValue={getSuggestionValue}
-					renderSuggestion={renderSuggestion}
-					inputProps={inputProps} />
+				<div id="box">
+					<h2>Please select the city you are in</h2>
+					<div id="chooser">
+					<h4>You can then begin to ask the questions you need</h4>
 					
-					<NavLink to="/city"><button onClick={this.onChangeCity.bind(this)} className="my-btn">GO</button></NavLink>
+					<Autosuggest 
+						suggestions={suggestions}
+						onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+						onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+						getSuggestionValue={getSuggestionValue}
+						renderSuggestion={renderSuggestion}
+						inputProps={inputProps} />
+						
+						<NavLink to="/city"><button onClick={this.onChangeCity.bind(this)} className="my-btn">GO</button></NavLink>
+					</div>
 				</div>
-				</div>
-				<p>You're new to a city? Come talk to people who know the city and easily get around!</p>
+				<p>You&#39;re new to a city? Come talk to people who know the city and easily get around!</p>
 				<p>We want to create a community of people that can help each other with what they have to offer. 
 				So don't doubt to find help here and to meet new people.</p>
 			</div>
