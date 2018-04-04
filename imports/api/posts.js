671b306faee3ba3db2ca208cc6c4f2056a1308f9
Fandresv26 <fa.vera10@uniandes.edu.co>
2018-04-04 16:47:23 -0500

    Code Review

--- imports/api/posts.js
+++ imports/api/posts.js
@@ -5,3 +5,5 @@ export const Posts = new Mongo.Collection("posts");
 export const Comments = new Mongo.Collection("comments");
 export const Chats = new Mongo.Collection("chats");
 export const ChatMessages = new Mongo.Collection("chatMessages");
+
+//F. Andres Vera: Les recomiendo usar meteor methods.
